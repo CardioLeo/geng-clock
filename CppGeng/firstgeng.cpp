90804e0978e5f2021e6d38ef7af9e3ca25c94aa8
echocslsk <CardioLeo@gmail.com>
2023-03-17 02:04:10 -0600

    adds a first draft of a cpp program for the geng clock; as yet barely tested, not working

---
+++ CppGeng/firstgeng.cpp
@@ -0,0 +1,14 @@
+//
+//
+//
+//
+//
+
+#include <iostream>
+
+int main() {
+	std::cout << "geng-clock started" << std::endl;
+	std::cout << "[actual clock content here]" << std::endl;
+	std::cout << "geng-clock finished" << std::endl;
+return 0;
+}
