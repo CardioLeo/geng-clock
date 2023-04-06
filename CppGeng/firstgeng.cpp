98b36ebf9b30bd17d25c44c1c96e9757a6ddf4a7
echocslsk <CardioLeo@gmail.com>
2023-04-06 10:15:47 -0600

    some new lines, incomplete, commented out though because incomplete; even the lines that are older and not commented out do not work, becayse requested include file not found

--- CppGeng/firstgeng.cpp
+++ CppGeng/firstgeng.cpp
@@ -1,10 +1,13 @@
-//
-//
-//
-//
-//
+// Author: William Z Chadwick
+// Date Created: 02/01/2023
+// Date Modified: 04/02/2023
+// Description:
+// 
 
+#include <ctime>
 #include <iostream>
+
+
 
 int main() {
 	std::cout << "geng-clock started" << std::endl;
@@ -12,3 +15,20 @@ int main() {
 	std::cout << "geng-clock finished" << std::endl;
 return 0;
 }
+
+
+
+/**/
+
+
+
+/*
+int main() {
+	std::clock_t start;
+	double geng;
+	start = ( std::clock() - start ) / (double)CLOCKS_PER_SEC;
+	geng = ;
+	std::cout << "It has been one geng since you started the clock!" << /n;
+	return 0;
+}
+*/
